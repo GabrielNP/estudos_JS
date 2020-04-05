0b5b96e55af10e4cb6443dee40dcb13a391f31c7
gabriel.novaes <gabriel.novaes@fs.com.br>
2020-04-05 20:16:25 -0300

    Otimizando o setter

--- avancado/aluraframe/client/js/app/services/ProxyFactory.js
+++ avancado/aluraframe/client/js/app/services/ProxyFactory.js
@@ -1,18 +1,29 @@
+/**
+ * Este design pattern provê uma abstração dos procedimentos de criação de um objeto, nos poupando de utilizar o operador new.
+ * Normalmente solicitamos a criação dos objetos à factory através de um método create, 
+ * e internamente ela faz todo o procedimento necessário para montagem do mesmo, facilitando a criação do objeto.
+ * 
+ * As factorys são particularmente úteis quando utilizadas na criação de objetos complexos, como é o caso dos nossos Proxys. 
+ * Também são úteis quando precisamos gerar objetos ligeiramente diferentes, podendo passar apenas estas pequenas modificações como argumento da factory,
+ * e ela gerará um objeto correspondente.
+*/
+
 class ProxyFactory {
 
     static create(objeto, props, acao) {
 
         return new Proxy(objeto, {
 
-            get(target, prop, receiver) {
+            get (target, prop, receiver) {
 
                 if (props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
 
                     return function() {
 
                         console.log(`Interceptando ${prop}`);
-                        Reflect.apply(target[prop], target, arguments);
-                        return acao(target);
+                        let retorno = Reflect.apply(target[prop], target, arguments);
+                        acao(target);
+                        return retorno;
                     }
                 }
 
@@ -21,12 +32,11 @@ class ProxyFactory {
 
             set (target, prop, value, receiver) {
 
-                if (props.includes(prop)) {
-                    target[prop] = value;
-                    acao(target);
-                }
+                let retorno = Reflect.set(target, prop, value, receiver);
+
+                if (target[prop] = value) acao(target);
                 
-                return Reflect.set(target, prop, value, receiver);
+                return retorno;
             }
         });
 
